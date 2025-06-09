426dbf6ffb776b314823a436ebc8643ec66338b6
Stephanie <webdesignfiap@gmail.com>
2025-06-09 19:54:10 -0300

    Operadores Relacionais

---
+++ Operadores relacionais/js/relacionais.js
@@ -0,0 +1,37 @@
+let numero1 = 10;
+let numero2 = `20`;
+console.log(`Primeiro número: ${numero1}`);
+console.log(`Segundo número: ${numero2}`);
+let resultado;
+
+//igualdade simples ==
+resultado = numero1 == numero2
+console.log(`${numero1} == ${numero2} = ${resultado}`);
+
+//desigualdade
+resultado = numero1 != numero2
+console.log(`${numero1} == ${numero2} = ${resultado}`);
+
+//igualdade estrita 
+resultado = numero1 === numero2
+console.log(`${numero1} === ${numero2} = ${resultado}`);
+
+//desigualdade estrita
+resultado = numero1 !== numero2
+console.log(`${numero1} !== ${numero2} = ${resultado}`);
+
+//maior >
+resultado = numero1 > numero2
+console.log(`${numero1} > ${numero2} = ${resultado}`);
+
+//maior ou igual a >=
+resultado = numero1 >= numero2
+console.log(`${numero1} >= ${numero2} = ${resultado}`);
+
+//menor <
+resultado = numero1 < numero2
+console.log(`${numero1} < ${numero2} = ${resultado}`);
+
+//menor ou igual a <=
+resultado = numero1 <= numero2
+console.log(`${numero1} <= ${numero2} = ${resultado}`);
