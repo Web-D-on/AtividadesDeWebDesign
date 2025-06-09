6d7ab5f435a07d57c92699528de0bb63fcf609ea
Stephanie <webdesignfiap@gmail.com>
2025-06-09 19:14:03 -0300

    Template String

---
+++ Template String/js/template.js
@@ -0,0 +1,20 @@
+let nome = 'Superman';
+let poder = 'Voaar';
+let estaVoando = false
+
+console.log(nome);
+console.log(poder);
+console.log(estaVoando);
+
+//  template 
+console.log(`Nome: ${nome}, ele tem como poder ${poder}. Ele está voando? ${estaVoando}`);
+
+//dentro de uma variavel 
+let mensagem = (`Nome: ${nome}, ele tem como poder ${poder}. Ele está voando? ${estaVoando}.
+Fim da template.`);
+console.log(mensagem);
+
+
+
+
+
