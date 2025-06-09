31473100365ec19ee63778887143921067a6983f
Stephanie <webdesignfiap@gmail.com>
2025-06-09 19:25:29 -0300

    Alert - Prompt - Comfirm

---
+++ Caixas de diálogo/js/caixas.js
@@ -0,0 +1,13 @@
+//alert
+alert('Olá sou um alerta');
+
+//prompt
+let = mensagem = prompt("Entre com uma mensagem");
+
+//exibir a mensagem
+console.log(`Mensagem digitada: ${mensagem}`);
+
+//comfirm
+let resposta = confirm('Deseja finalizar o aplicativo');
+console.log(resposta);
+
