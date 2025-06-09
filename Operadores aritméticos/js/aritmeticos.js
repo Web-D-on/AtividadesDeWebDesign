7017b8fcad0dd6ed1dbec9b02a0318899a2acd8a
Stephanie <webdesignfiap@gmail.com>
2025-06-09 19:37:38 -0300

    Operadores Aritméticos

---
+++ Operadores aritméticos/js/aritmeticos.js
@@ -0,0 +1,26 @@
+let primeiroNumero = 50;
+let segundoNumero = 8;
+let resultado;
+
+console.log(`Primeiro Número = ${primeiroNumero}`);
+console.log(`Segundo Número = ${segundoNumero}`);
+
+//adição
+resultado = primeiroNumero + segundoNumero;
+console.log(`${primeiroNumero} + ${segundoNumero} = ${resultado}`);
+
+//subtração 
+resultado = primeiroNumero - segundoNumero;
+console.log(`${primeiroNumero} - ${segundoNumero} = ${resultado}`);
+
+//divisão
+resultado = primeiroNumero / segundoNumero;
+console.log(`${primeiroNumero} / ${segundoNumero} = ${resultado}`);
+
+//multiplicação
+resultado = primeiroNumero - segundoNumero;
+console.log(`${primeiroNumero} - ${segundoNumero} = ${resultado}`);
+
+//modulo
+resultado = primeiroNumero % segundoNumero;
+console.log(`${primeiroNumero} % ${segundoNumero} = ${resultado}`);
