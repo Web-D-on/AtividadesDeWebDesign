a19ed9164c1223d1905f62c64ec08d7d04ce6ba1
Stephanie <webdesignfiap@gmail.com>
2025-06-11 19:51:59 -0300

    Operadores de Atribuição

---
+++ Operadores de atribuição/js/atribuicao.js
@@ -0,0 +1,24 @@
+let primeiroNumero = 100;
+
+console.log(`Primeiro Número: ${primeiroNumero}`);
+primeiroNumero+=10;
+console.log(`Primeiro Número: ${primeiroNumero}`);
+
+primeiroNumero-=10;
+console.log(`Primeiro Número: ${primeiroNumero}`);
+
+primeiroNumero*=10;
+console.log(`Primeiro Número: ${primeiroNumero}`);
+
+primeiroNumero/=10;
+console.log(`Primeiro Número: ${primeiroNumero}`);
+
+primeiroNumero**=10;
+console.log(`Primeiro Número: ${primeiroNumero}`);
+
+primeiroNumero%=10;
+console.log(`Primeiro Número: ${primeiroNumero}`);
+
+
+
+
